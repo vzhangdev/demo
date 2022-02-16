928eb15c4600ed8ba4daad220959bf0251d74757
Vincent <vzhang@fintechautomation.com>
2022-02-16 17:47:47 -0600

    init

--- src/index.js
+++ src/index.js
@@ -8,10 +8,6 @@ ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('like_button_container')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
